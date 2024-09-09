c6c057fd120dfd0475bd79a38ab9ee1f6a5aaace
Atlanical <michaeldance@ticaga.com>
2024-09-09 21:01:22 +0100

    updates

---
+++ ticaga_blesta_plugin.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Ticaga's Blesta plugin handler
+ *
+ * @package blesta
+ * @subpackage blesta.plugins.ticaga_blesta
+ * @copyright Copyright (c) 2010, Phillips Data, Inc.
+ * @copyright Copyright (c) 2024, Ticaga Ltd.
+ * @license http://www.blesta.com/license/ The Blesta License Agreement
+ * @link http://www.blesta.com/ Blesta
+ * @link http://www.ticaga.com/ Ticaga
+ */
+class TicagaBlestaPlugin extends Plugin
+{
+    public function __construct()
+    {
+        $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
+        Language::loadLang('ticaga_blesta', null, dirname(__FILE__) . DS . 'language' . DS);
+    }
+
+    /**
+     * Performs any necessary bootstraping actions
+     *
+     * @param int $plugin_id The ID of the plugin being installed
+     */
+    public function install($plugin_id)
+    {
+        if (!isset($this->Record)) {
+            Loader::loadComponents($this, ['Input', 'Record']);
+        }
+
+        try {
+
+        } catch (Exception $e) {
+            // Error adding... no permission?
+            $this->Input->setErrors(['db' => ['create' => $e->getMessage()]]);
+            return;
+        }
+    }
+
+    /**
+     * Performs any necessary cleanup actions
+     *
+     * @param int $plugin_id The ID of the plugin being uninstalled
+     * @param bool $last_instance True if $plugin_id is the last instance
+     * across all companies for this plugin, false otherwise
+     */
+    public function uninstall($plugin_id, $last_instance)
+    {
+        if (!isset($this->Record)) {
+            Loader::loadComponents($this, ['Record']);
+        }
+
+        if ($last_instance) {
+            try {
+                $this->Record->drop('ticaga_blesta');
+            } catch (Exception $e) {
+                // Error dropping... no permission?
+                $this->Input->setErrors(['db'=> ['create'=>$e->getMessage()]]);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Performs migration of data from $current_version (the current installed version)
+     * to the given file set version
+     *
+     * @param string $current_version The current installed version of this plugin
+     * @param int $plugin_id The ID of the plugin being upgraded
+     */
+    public function upgrade($current_version, $plugin_id)
+    {
+        if (!isset($this->Record)) {
+            Loader::loadComponents($this, ['Record']);
+        }
+
+        // Upgrade if possible
+        if (version_compare($this->getVersion(), $current_version, '>')) {
+
+        }
+    }
+
+    /**
+     * Returns all actions to be configured for this widget
+     * (invoked after install() or upgrade(), overwrites all existing actions)
+     *
+     * @return array A numerically indexed array containing:
+     *  - action The action to register for
+     *  - uri The URI to be invoked for the given action
+     *  - name The name to represent the action (can be language definition)
+     */
+    public function getActions()
+    {
+        return [];
+    }
+
+}
