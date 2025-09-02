f819bc9919429f40a2d15040172667afba74aaa1
Michael Dance <119854202+Ticaga-Mike@users.noreply.github.com>
2025-09-02 16:07:57 +0100

    Updated language files

--- ticaga_support/language/en_us/ticaga_support_blesta_settings.php
+++ ticaga_support/language/en_us/ticaga_support_blesta_settings.php
@@ -1,6 +1,6 @@
 <?php
 
 // Errors
-$lang['TicagaBlestaSettings.!error.api_key.valid'] = 'Invalid api_key';
-$lang['TicagaBlestaSettings.!error.api_url.valid'] = 'Invalid api_url';
-
+$lang['TicagaBlestaSettings.!error.api_key.valid'] = 'Invalid API Key';
+$lang['TicagaBlestaSettings.!error.api_url.valid'] = 'Invalid Ticaga Installation URL';
+$lang['TicagaBlestaSettings.!error.api_email.valid'] = 'Invalid Email';
